7ea07da3acc9fc860f848334a985a3e85ad7eb47
Nabamita123 <90511009+Nabamita123@users.noreply.github.com>
2024-01-15 20:45:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.getElementById('envelope-container').addEventListener('click', function() {
     this.classList.toggle('open');
   });
-  
+  function redirectToToDoList(response) {
+    if (response === 'yes' || response === 'Yes') {
+        window.location.href = 'todolist.html';
+   }
+}
